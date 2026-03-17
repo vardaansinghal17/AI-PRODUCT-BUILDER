18e5e798f225ffcd6700a977f879ee005168b0e8
Vardan Singhal <vardansinghal3808@gmail.com>
2026-03-17 23:29:19 +0530

    setup server

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import type { Application, Request, Response } from "express";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("AI Product Builder API is running...");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
