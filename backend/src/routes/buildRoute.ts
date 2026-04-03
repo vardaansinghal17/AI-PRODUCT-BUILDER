2d94808cc4ea62b734c5491f37c8ad6ef5cdd68e
Vardan Singhal <vardansinghal3808@gmail.com>
2026-04-03 23:26:53 +0530

    fix the server

--- backend/src/routes/buildRoute.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { buildProject } from "../controllers/buildController";
-
-const router = Router();
-
-router.post("/build", buildProject);
-
-export default router;
